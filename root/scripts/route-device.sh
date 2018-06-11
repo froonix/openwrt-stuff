4f52dcecad0c04720f17d8c0a4ee21c19cebd71e
Christian Schrötter <cs@fnx.li>
2018-06-11 18:16:11 +0200

    "ip rule add from <…> lookup <…>" helper script

---
+++ root/scripts/route-device.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euf -o pipefail
+
+if [[ "${1-}" == "" ]]
+then
+	echo "Usage: $0 <DEVICE> [<TABLE> [<MAIN>]]" 1>&2
+	echo "Route DEVICE (ip/subnet) via TABLE. Creates source routing rules." 1>&2
+fi
+
+ip -4 rule add from "$1" to 10.0.0.0/8     priority 20001 lookup "${3:-main}" && \
+ip -4 rule add from "$1" to 172.16.0.0/12  priority 20001 lookup "${3:-main}" && \
+ip -4 rule add from "$1" to 192.168.0.0/16 priority 20001 lookup "${3:-main}" && \
+ip -4 rule add from "$1"                   priority 20002 lookup "${2:-wan}"  && \
+
+ip -4 route flush cache
+exit $?
