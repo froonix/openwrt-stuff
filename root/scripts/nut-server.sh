cdd8633e6790939b301111932706d457623be126
Christian Schrötter <cs@fnx.li>
2021-08-02 17:49:33 +0200

    Update NUT restart script
    Note: Package nut-upsc required now!
    
    Replace "ups" at the first command with your UPS name.

--- root/scripts/nut-server.sh
+++ root/scripts/nut-server.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-PIDFILE="/var/run/apcupsd-ups-ups.pid"
-PARENTPIDFILE="/var/run/upsd.pid"
-
-if [ -f "$PIDFILE" ]
-then
-	kill -0 "$(cat "$PIDFILE")" 2>/dev/null
-	if [ "$?" -eq "0" ]; then exit 0; fi
-fi
-
-if [ ! -f "$PARENTPIDFILE" ]; then exit 2; fi
-kill -0 "$(cat "$PARENTPIDFILE")" 2>/dev/null
-if [ ! "$?" -eq "0" ]; then exit 2; fi
-
-logger -s "Restarting NUT server..."
-/etc/init.d/nut-server.init restart
-exit 1
+/usr/bin/upsc ups || /etc/init.d/nut-server restart
